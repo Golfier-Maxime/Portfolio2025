0326423216b48effca94c34cabc37e7f8fe0bcce
Maxime Golfier <maxime.golfier@outlook.fr>
2025-07-18 10:15:19 +0200

    header

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,13 +1,17 @@
 <template>
-    <div class="bg-topbar"></div>
-    <div>
-        <div>
-            <nuxt-link to="/">
-                <img src=" ../public/Logo_MG.svg" alt="Logo" class="logo">
-            </nuxt-link>
+    <div class="header">
+        <div class="bg-topbar"></div>
+        <div class="header-content">
+            <div class="header-content_logo">
+                <nuxt-link to="/">
+                    <img src=" ../public/Logo_MG.svg" alt="Logo">
+                </nuxt-link>
+            </div>
+            <div class="header-content_menu">
+                <nuxt-link to="/project">Project</nuxt-link>
+                <nuxt-link to="/about">About</nuxt-link>
+                <nuxt-link to="/contact">Contact</nuxt-link>
+            </div>
         </div>
-        <nuxt-link to="/" class="color">Home</nuxt-link>
-        <nuxt-link to="/project">project</nuxt-link>
-        <nuxt-link to="/">Home</nuxt-link>
     </div>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,25 @@
 <template>
     <div>
         <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
     </div>
 </template>
